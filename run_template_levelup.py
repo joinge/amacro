773fbced2d74f7e8b773a4ceb402c5e859d0d4bd
Jo Inge Buskenes <mail@joinge.net>
2013-07-29 23:55:50 +0200

    Added skillup template

---
+++ run_template_levelup.py
@@ -0,0 +1,14 @@
+from macro import *
+
+def run():
+
+  setActiveDevice('192.168.10.10:5555')
+  user.setCurrent("Joey")
+
+  startMarvel('--TheLegend--', password='chrisb51383')
+  skillUpAccount()
+
+if __name__ == "__main__":
+
+   run()
+
